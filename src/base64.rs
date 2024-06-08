5ebba042717c0b057337e2b090873e84fc031ea3
BasilBenAziz <basilbenaziz@gmail.com>
2024-06-08 21:35:14 +0300

    Refactor base64.rs to improve code readability and add base64 decoding functionality

--- src/base64.rs
+++ src/base64.rs
@@ -1,58 +1,3 @@
-pub fn base64_char_to_val(c: char) -> Option<u8> {
-    match c {
-        'A'..='Z' => Some(c as u8 - 'A' as u8),
-        'a'..='z' => Some(c as u8 - 'a' as u8 + 26),
-        '0'..='9' => Some(c as u8 - '0' as u8 + 52),
-        '+' => Some(62),
-        '/' => Some(63),
-        _ => None,
-    }
-}
-
-
-
-
-
-
-
-
-pub fn decode_base64(encoded: &str) -> Result<Vec<u8>, &'static str> {
-    let encoded = encoded.trim_end_matches('=');
-
-
-    // if encoded.len() % 4 != 0 {
-        // return Err("Invalid Base64 string length");
-    // }
-
-
-
-    let mut decoded_bytes: Vec<u8> = Vec::new();
-    let mut buffer = 0u32;
-    let mut bits_collected = 0;
-
-
-
-    for c in encoded.chars() {
-        let val = match base64_char_to_val(c) {
-            Some(val) => val,
-            None => return Err("Invalid character in Base64 string"),
-        };
-        buffer = (buffer << 6) | (val as u32);
-        bits_collected += 6;
-
-        if bits_collected >= 8 {
-            bits_collected -= 8;
-            decoded_bytes.push((buffer >> bits_collected) as u8);
-            buffer &= (1 << bits_collected) - 1;
-        }
-    }
-
-    Ok(decoded_bytes)
-}
-
-
-
-
 
 
 
@@ -65,5 +10,69 @@ pub fn is_base64(s: &str) -> bool {
     if s.len() % 4 != 0 {
         return false;
     }
-    s.chars().all(|c| c.is_ascii_alphanumeric() || c == '+' || c == '/' || c == '=')
+    s.chars()
+        .all(|c| c.is_ascii_alphanumeric() || c == '+' || c == '/' || c == '=')
+}
+
+
+
+
+
+
+
+
+pub fn decode_base64_string(mut encoded_str: Option<String>) {
+    while let Some(ref s) = encoded_str {
+
+        if is_base64(s) {
+            match decode_base64(s) {
+                Ok(decoded_bytes) => {
+                    let decoded_str = String::from_utf8(decoded_bytes).unwrap();
+                    println!("from Base64 :{}", decoded_str);
+                    encoded_str = Some(decoded_str);
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        }else {
+            break;
+        }
+        
+    }
+}
+
+
+pub fn decode_base64(encoded: &str) -> Result<Vec<u8>, &'static str> {
+    let encoded = encoded.trim_end_matches('=');
+    let mut decoded_bytes: Vec<u8> = Vec::new();
+    let mut buffer = 0u32;
+    let mut bits_collected = 0;
+
+
+    for c in encoded.chars() {
+        let val = match base64_char_to_val(c) {
+            Some(val) => val,
+            None => return Err("Invalid character in Base64 string"),
+        };
+        buffer = (buffer << 6) | (val as u32);
+        bits_collected += 6;
+        if bits_collected >= 8 {
+            bits_collected -= 8;
+            decoded_bytes.push((buffer >> bits_collected) as u8);
+            buffer &= (1 << bits_collected) - 1;
+        }
+    }
+    Ok(decoded_bytes)
+}
+
+pub fn base64_char_to_val(c: char) -> Option<u8> {
+    match c {
+        'A'..='Z' => Some(c as u8 - 'A' as u8),
+        'a'..='z' => Some(c as u8 - 'a' as u8 + 26),
+        '0'..='9' => Some(c as u8 - '0' as u8 + 52),
+        '+' => Some(62),
+        '/' => Some(63),
+        _ => None,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
 mod base64;
-mod hex;
-mod url;
+
+
+
+
+
 
 fn main() {
-    let encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9"; // Example input, replace with your string
+    // let mut encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9";
+    let encoded_str = Some("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());
 
-    if base64::is_base64(encoded_str) {
+    base64::decode_base64_string(encoded_str);
 
-        match base64::decode_base64(encoded_str) {
-            Ok(decoded) => {
-                match String::from_utf8(decoded) {
-                    Ok(decoded_string) => println!("Decoded Base64 string: {}", decoded_string),
-                    Err(_) => println!("Failed to convert decoded Base64 bytes to string"),
-                }
-            },
-            Err(e) => println!("Error: {}", e),
-        }
-
-        
-
-
-
-
-        
-    } else if hex::is_hex(encoded_str) {
-        match hex::decode_hex(encoded_str) {
-            Ok(decoded) => {
-                match String::from_utf8(decoded) {
-                    Ok(decoded_string) => println!("Decoded Hex string: {}", decoded_string),
-                    Err(_) => println!("Failed to convert decoded Hex bytes to string"),
-                }
-            },
-            Err(e) => println!("Error: {}", e),
-        }
-
-
-
-
-    } else if url::is_url_encoded(encoded_str) {
-        match url::decode_url(encoded_str) {
-            Ok(decoded_string) => println!("Decoded URL string: {}", decoded_string),
-            Err(e) => println!("Error: {}", e),
-        }
-    } else {
-        println!("The string is neither valid Base64, Hex, nor URL encoded.");
-    }
+    
 }
+
+
