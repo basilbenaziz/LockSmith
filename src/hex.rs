b66696755fade6adedaa759e0a3a6cf87dc90d6d
BasilBenAziz <basilbenaziz@gmail.com>
2024-06-08 17:59:19 +0300

    Refactor main.rs to improve code readability and remove unnecessary whitespace

--- src/hex.rs
+++ src/hex.rs
@@ -1,17 +1,3 @@
-/// The function `hex_char_to_val` converts a hexadecimal character to its corresponding numerical
-/// value.
-/// 
-/// Arguments:
-/// 
-/// * `c`: The function `hex_char_to_val` takes a single character `c` as input and returns an
-/// `Option<u8>`. The function converts a hexadecimal character to its corresponding numerical value.
-/// 
-/// Returns:
-/// 
-/// The function `hex_char_to_val` takes a character as input and returns an `Option<u8>`. The function
-/// matches the input character against different ranges of characters ('0' to '9', 'a' to 'f', 'A' to
-/// 'F') and converts them to their corresponding hexadecimal values. If the input character is within
-/// one of these ranges, the function returns `Some(value
 pub fn hex_char_to_val(c: char) -> Option<u8> {
     match c {
         '0'..='9' => Some(c as u8 - '0' as u8),
@@ -21,25 +7,6 @@ pub fn hex_char_to_val(c: char) -> Option<u8> {
     }
 }
 
-
-
-
-
-
-
-/// The `decode_hex` function in Rust decodes a hexadecimal encoded string into a vector of bytes.
-/// 
-/// Arguments:
-/// 
-/// * `encoded`: The function `decode_hex` takes a hexadecimal encoded string as input and decodes it
-/// into a vector of bytes. The input parameter `encoded` is the hexadecimal string that needs to be
-/// decoded.
-/// 
-/// Returns:
-/// 
-/// The function `decode_hex` returns a `Result` enum with the success variant containing a `Vec<u8>`
-/// representing the decoded bytes if the decoding process is successful, and the error variant
-/// containing a `&'static str` with an error message if any issues occur during decoding.
 pub fn decode_hex(encoded: &str) -> Result<Vec<u8>, &'static str> {
     if encoded.len() % 2 != 0 {
         return Err("Invalid Hex string length");
@@ -63,22 +30,6 @@ pub fn decode_hex(encoded: &str) -> Result<Vec<u8>, &'static str> {
     Ok(decoded_bytes)
 }
 
-
-
-
-
-/// The function `is_hex` in Rust checks if a given string is a valid hexadecimal representation.
-/// 
-/// Arguments:
-/// 
-/// * `s`: The parameter `s` is a reference to a string slice (`&str`) that represents a hexadecimal
-/// string. The function `is_hex` checks if the input string is a valid hexadecimal string by ensuring
-/// that it has an even length and that all characters are valid hexadecimal digits.
-/// 
-/// Returns:
-/// 
-/// The function `is_hex` returns a boolean value indicating whether the input string `s` is a valid
-/// hexadecimal string.
 pub fn is_hex(s: &str) -> bool {
     if s.len() % 2 != 0 {
         return false;
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@ mod hex;
 mod url;
 
 fn main() {
-    //Base64//SGVsbG8gV29ybGQh
-    //Base64////Base64//U0dWc2JHOGdWMjl5YkdRaA==
-    //HEX//68 69 20 6d 65
-    let encoded_str = "U0dWc2JHOGdWMjl5YkdRaA=="; // Example input, replace with your string
-
-
-
+    let encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9"; // Example input, replace with your string
 
     if base64::is_base64(encoded_str) {
+
         match base64::decode_base64(encoded_str) {
             Ok(decoded) => {
                 match String::from_utf8(decoded) {
@@ -22,9 +17,12 @@ fn main() {
             Err(e) => println!("Error: {}", e),
         }
 
+        
 
 
 
+
+        
     } else if hex::is_hex(encoded_str) {
         match hex::decode_hex(encoded_str) {
             Ok(decoded) => {
@@ -39,19 +37,12 @@ fn main() {
 
 
 
-
     } else if url::is_url_encoded(encoded_str) {
         match url::decode_url(encoded_str) {
             Ok(decoded_string) => println!("Decoded URL string: {}", decoded_string),
             Err(e) => println!("Error: {}", e),
         }
-
-
-
     } else {
         println!("The string is neither valid Base64, Hex, nor URL encoded.");
     }
-
-
-
 }
