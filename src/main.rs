3086b3e5c91aa97843367c79e861bc2df8b65940
BasilBenAziz <basilbenaziz@gmail.com>
2024-06-09 16:36:38 +0300

    Refactor main.rs to improve code readability and remove unnecessary whitespace

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,12 @@ mod base64;
 
 
 
+
 fn main() {
-    // let mut encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9";
-    let encoded_str = Some("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());
-
-    base64::decode_base64_string(encoded_str);
+  // let mut encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9";
+  let encoded_str = Some("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());
+  
     
-
+    base64::decode_base64_string(encoded_str);
 
 }
-
-
