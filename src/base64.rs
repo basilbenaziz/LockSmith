6f957e2a7bab3b9291398d3fcfe12690166c3054
BasilBenAziz <basilbenaziz@gmail.com>
2024-06-10 19:49:20 +0300

    Refactor main.rs to improve code readability and remove unnecessary whitespace

--- src/base64.rs
+++ src/base64.rs
@@ -1,68 +1,91 @@
 
-pub fn is_base64(s: &str) -> bool {
-    if s.len() % 4 != 0 {
-        return false;
-    }
-    s.chars()
-        .all(|c| c.is_ascii_alphanumeric() || c == '+' || c == '/' || c == '=')
+
+pub struct Base64 {
+    encoded_str: String,
 }
+
+impl Base64 {
+    /// The function `new` creates a new instance of the `Base64` struct with the provided encoded
+    /// string.
+    /// 
+    /// Arguments:
+    /// 
+    /// * `encoded_str`: The `encoded_str` parameter in the `new` function is a `String` type that
+    /// represents the base64 encoded string that will be used to create a new instance of the `Base64`
+    /// struct.
+    /// 
+    /// Returns:
+    /// 
+    /// A new instance of the `Base64` struct with the `encoded_str` field set to the provided `String`
+    /// value.
+    pub fn new(encoded_str: String) -> Base64 {
+        Base64 { encoded_str }
+    }
+
+    /// The function `is_base64` checks if all characters in the encoded string are valid Base64
+    /// characters.
+    /// 
+    /// Returns:
+    /// 
+    /// The `is_base64` method returns a boolean value indicating whether the encoded string is a valid
+    /// Base64 string. It checks if the length of the encoded string is a multiple of 4 and if all
+    /// characters in the string are either ASCII alphanumeric characters or one of the characters '+',
+    /// '/', or '='. If these conditions are met, the method returns `true`, indicating that the string
+    /// is a valid
+    fn is_base64(&self) -> bool {
+        if self.encoded_str.len() % 4 != 0 {
+            return false;
+        }
+        // This code snippet is checking if all characters in the encoded string are either ASCII
+        // alphanumeric characters or one of the characters '+', '/', or '='.
+        self.encoded_str
+            .chars()
+            .all(|c| c.is_ascii_alphanumeric() || c == '+' || c == '/' || c == '=')
+    }
+
+
+/// The function `base64_decode` decodes a Base64 encoded string and returns the decoded string.
+/// 
+/// Returns:
+/// 
+/// The `base64_decode` function returns the decoded Base64 string as a `String`.
+pub fn base64_decode(&mut self) -> String {
     
+    while  self.is_base64() {
+        let base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        let mut output = Vec::new();
+        let mut buffer = 0u32;
+        let mut bits_collected = 0;
 
-
-
-pub fn decode_base64_string(mut encoded_str: Option<String>) {
-    while let Some(ref s) = encoded_str {
-
-        if is_base64(s) {
-            match decode_base64(s) {
-                Ok(decoded_bytes) => {
-                    let decoded_str = String::from_utf8(decoded_bytes).unwrap();
-                    println!("from Base64 :{}", decoded_str);
-                    encoded_str = Some(decoded_str);
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                }
+        for c in self.encoded_str.chars() {
+            if c == '=' {
+                break;
             }
-        }else {
-            break;
+
+            let value = match base64_chars.find(c) {
+                Some(val) => val as u32,
+                None => return "Invalid character in Base64 string".to_string(),
+            };
+
+            buffer = (buffer << 6) | value;
+            bits_collected += 6;
+
+            if bits_collected >= 8 {
+                bits_collected -= 8;
+                let byte = (buffer >> bits_collected) as u8;
+                output.push(byte);
+                buffer &= (1 << bits_collected) - 1;
+            }
         }
+
+
         
+        self.encoded_str = String::from_utf8(output).unwrap();
+        print!("Decode Base64: {}\n", self.encoded_str);
     }
+    
+    return self.encoded_str.to_string();
+    
 }
 
-
-pub fn decode_base64(encoded: &str) -> Result<Vec<u8>, &'static str> {
-    let encoded = encoded.trim_end_matches('=');
-    let mut decoded_bytes: Vec<u8> = Vec::new();
-    let mut buffer = 0u32;
-    let mut bits_collected = 0;
-
-    for c in encoded.chars() {
-        
-        let val = match base64_char_to_val(c) {
-            Some(val) => val,
-            None => return Err("Invalid character in Base64 string"),
-        };
-        buffer = (buffer << 6) | (val as u32);
-        bits_collected += 6;
-        if bits_collected >= 8 {
-            bits_collected -= 8;
-            decoded_bytes.push((buffer >> bits_collected) as u8);
-            buffer &= (1 << bits_collected) - 1;
-        }
-    }
-    Ok(decoded_bytes)
-}
-
-
-pub fn base64_char_to_val(c: char) -> Option<u8> {
-    match c {
-        'A'..='Z' => Some(c as u8 - 'A' as u8),
-        'a'..='z' => Some(c as u8 - 'a' as u8 + 26),
-        '0'..='9' => Some(c as u8 - '0' as u8 + 52),
-        '+' => Some(62),
-        '/' => Some(63),
-        _ => None,
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 // mod base64;
-
-mod base64_test;
-use base64_test::Base64;
+mod base64;
+use base64::Base64;
 
 
 
 
 fn main() {
-    let encoded_str = Base64::new("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());
-    print!("{}\n", encoded_str.is_base64());
-    
-    
-    // let mut encoded_str = "U0dWc2JHOGdVMjVuY0dWdWRGOTFjR1Z5ZG1sa1pXNTBhWFI1TG1OdmJTOWhhV1E9";
-    // let encoded_str = Some("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());    
-    
-    // base64::decode_base64_string(encoded_str);
+    let mut encoded = Base64::new("VmpGYVYyRXhXWGxVV0d4VVlUSm9VVlZyVWtKUFVUMDk=".to_string());
+    print!("{}\n", encoded.base64_decode());
 }
